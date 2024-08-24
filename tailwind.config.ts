9c49c80ec8abb4af778ee887c847bc3a34d83215
Paweł <pawel.stach81@gmail.com>
2024-08-24 09:25:44 +0200

    Update .gitignore

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,28 @@
 import type { Config } from "tailwindcss";
 
+import plugin from "tailwindcss/plugin";
+
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/{app,ui,components}/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        ink: "#000",
+        canvas: "#fff",
+
+        accent: "#000",
+      },
+      fontFamily: {},
+      maxHeight: {
+        fold: "calc(100svh - var(--header-height))",
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addVariant }) {
+      addVariant("header-open", "body:has(#header-open:checked) &");
+      addVariant("header-closed", "body:has(#header-open:not(:checked)) &");
+    }),
+  ],
 };
 export default config;

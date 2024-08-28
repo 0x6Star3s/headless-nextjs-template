54cda9615bc258ea12213a8853c505dbb53c2bf8
Paweł <pawel.stach81@gmail.com>
2024-08-28 09:11:58 +0200

    add component Gallery

---
+++ src/components/modules/dev/Gallery.dev.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import { useState } from "react";
+import Img from "@/components/ui/Img";
+import ImageZoomSwiper from "@/components/ui/Image-zoomSwiper";
+import { uid } from "@/lib/uid";
+
+function GalleryDev({
+  pretitle,
+  title,
+  imagesGroupArray,
+  ...props
+}: Sanity.Module &
+  Partial<{
+    pretitle: string;
+    title: string;
+    imagesGroupArray: {
+      image: Sanity.Image;
+    }[];
+  }>) {
+  const [model, setModel] = useState<boolean>(false);
+  const [activeIndex, setActiveIndex] = useState<number | null>(null);
+
+  const handleModelOpen = (index: number) => {
+    setModel((pev) => !pev);
+    setActiveIndex(index);
+    window.document.body.classList.add("hidden-scroll");
+  };
+
+  const handleModelClose = () => {
+    setModel((pev) => !pev);
+    window.document.body.classList.remove("hidden-scroll");
+  };
+
+  return (
+    <section
+      id={uid(props)}
+      className="flex flex-wrap justify-center space-x-10 py-[5rem] px-5 bg-[#f3f5f9]"
+    >
+      <div className="max-w-[1000px] w-[90%] mx-auto">
+        {pretitle && (
+          <span>
+            <h3 className="text-[#130a4f] font-medium uppercase">{pretitle}</h3>
+            <div className="bg-black w-[50px] h-[1.3px]" />
+          </span>
+        )}
+
+        {title && (
+          <h2 className="text-[#11181c] text-4xl font-bold mt-3">{title}</h2>
+        )}
+        {(imagesGroupArray?.length as any) >= 1 && (
+          <div className="columns-1 md:columns-2 lg:columns-3 space-y-5 mt-5">
+            {imagesGroupArray?.map((item, index) => (
+              <div
+                className="cursor-pointer hover:opacity-80 transition-all duration-300"
+                key={index}
+                onClick={() => handleModelOpen(index)}
+              >
+                <Img className="w-full h-full rounded-lg" image={item.image} />
+              </div>
+            ))}
+          </div>
+        )}
+        {model ? (
+          <ImageZoomSwiper
+            images={imagesGroupArray}
+            index={activeIndex}
+            model={model}
+            handerModelClose={handleModelClose}
+          />
+        ) : null}
+      </div>
+    </section>
+  );
+}
+
+export default GalleryDev;
---
+++ src/components/ui/Image-zoomSwiper.tsx
@@ -0,0 +1,90 @@
+"use client";
+import { useState } from "react";
+import { cn } from "@/lib/utils";
+import Img from "@/components/ui/Img";
+
+import { IoCloseCircleOutline } from "react-icons/io5";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode, Navigation, Thumbs } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/navigation";
+import "swiper/css/thumbs";
+
+interface imageZoomSwiperProps {
+  images?: { image: Sanity.Image }[];
+  index: number | null;
+  model: boolean;
+  handerModelClose: () => void;
+}
+
+// window.document.body.classList.remove("over");
+
+export default function ImageZoomSwiper({
+  images,
+  index,
+  model,
+  handerModelClose,
+}: imageZoomSwiperProps) {
+  const [thumbsSwiper, setThumbsSwiper] = useState<any | null | undefined>(
+    null
+  );
+  const temp =
+    "fixed top-0 left-0 p-[2rem] md:p-[4rem] h-screen w-[100%] z-[900] ";
+
+  return (
+    <div className={model ? `${temp}` : cn(temp, "hidden")}>
+      <div
+        className="absolute bg-blend-screen bg-[rgba(235,235,239,0.84)] backdrop-filter backdrop-blur-md h-screen top-0 left-0 right-0 z-0"
+        onClick={handerModelClose}
+      ></div>
+      <button
+        className="absolute top-3 rounded-lg right-3 p-1 z-[1000] bg-zinc-200 border-zinc-300"
+        onClick={handerModelClose}
+      >
+        <IoCloseCircleOutline className="text-3xl text-zinc-500" />
+      </button>
+
+      <Swiper
+        spaceBetween={10}
+        navigation={true}
+        thumbs={{ swiper: thumbsSwiper }}
+        modules={[Navigation, Thumbs]}
+        initialSlide={index as number}
+        className="mainSwiper"
+      >
+        {images?.map((imgSrc, index) => (
+          <SwiperSlide key={index} className="z-10">
+            <div className="flex w-full items-center justify-center h-full rounded-lg overflow-hidden">
+              <Img
+                className="w-fit h-fit max-w-[1200px]"
+                image={imgSrc.image}
+              />
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <Swiper
+        onSwiper={setThumbsSwiper}
+        spaceBetween={10}
+        slidesPerView="auto"
+        freeMode={true}
+        initialSlide={index as number}
+        watchSlidesProgress={true}
+        modules={[FreeMode, Navigation, Thumbs]}
+        className="miniSwiper"
+      >
+        {images?.map((imgSrc, index) => (
+          <SwiperSlide key={index}>
+            <Img
+              className="hover:opacity-50 transition-all duration-300"
+              image={imgSrc.image}
+              alt=""
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      {/* </div> */}
+    </div>
+  );
+}
